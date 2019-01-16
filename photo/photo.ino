c6cb932dc579d31070d6f822cbdea922142477f1
AB <ultradesu@hexor.ru>
2019-01-16 21:45:54 +0300

    Improve photo sketch.

--- photo/photo.ino
+++ photo/photo.ino
@@ -8,30 +8,30 @@ U8G2_KS0108_128X64_F u8g2(U8G2_R0, 13, 12, 11, 10, 9, 8, 7, 6,
                           2, 3, 5, 4, U8X8_PIN_NONE, U8X8_PIN_NONE);
 
 
-short vals[127];
+char vals[127];
 void setup(void)
 {
   u8g2.begin();
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
-  for (int i = 0; i < 127; ++i) {
+  u8g2.setFont(u8g2_font_unifont_t_cyrillic);
+  for (short i = 0; i < 127; ++i) {
     vals[i] = 0;
   }
 }
-
+ 
 void loop(void)
 {
   u8g2.clearBuffer();
-  int raw, val, ui_val;
+short raw, val, ui_val;
   raw = analogRead(LDR_PIN);
   val = map(raw, 0, 1023, 50, 0);
   ui_val = map(raw, 0, 1023, 100, 0);  
   append(val);
-  char cstr[16];
+  char cstr[3];
   itoa(ui_val, cstr, 10);
-  u8g2.drawStr(0, 10, cstr);
-  for (int i = 0; i < 127; ++i) {
-    u8g2.drawBox(i, AXIS, 1, vals[i]);
+  u8g2.drawStr(0, 12, cstr);
+  for (char i = 0; i < 127; ++i) {
+    u8g2.drawBox(i, 63-vals[i], 1, vals[i]);
   }
   u8g2.sendBuffer();
   if (val <= 10) {
@@ -44,9 +44,9 @@ void loop(void)
 
 }
 
-void append(int value)
+void append(char value)
 {
-  for (int i = 1; i < 127; ++i) {
+  for (char i = 1; i < 127; ++i) {
     vals[i - 1] = vals[i];
   }
   vals[126] = value;
